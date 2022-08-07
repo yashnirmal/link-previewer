d6a17f69ac8823c72162f1166cafd72acce3d3ec
yash nirmal <yash15nirmal@gmail.com>
2022-08-07 15:14:35 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import { getLinkPreview, getPreviewFromContent } from "link-preview-js";
 
 function App() {
+
+  const [data,setData] = useState();
+
+  function fetchMetaData(){
+    console.log("fetching...");
+    let link = document.getElementById('link-input').value;
+
+    getLinkPreview(link).then((data) =>
+      console.log(data)
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" id='link-input' placeholder='Enter Link'/>
+      <button onClick={fetchMetaData}>Preview</button>
     </div>
   );
 }
